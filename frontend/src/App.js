c06929a49140e41d06b6f3c55923b0f76d952009
dreamxinxcode <dreamxinxcode@gmail.com>
2021-02-22 11:59:47 -0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Nav from ''
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.scss';
+
+function Nav() {
+  return (
+    <nav>
+
+    </nav>
+  )
+}
+
+export default Nav
